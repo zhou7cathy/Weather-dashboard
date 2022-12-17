7ed165589b6abc0a1ba44b1df462e1c71aefff51
unknown <zhou7cathy@gmail.com>
2022-12-17 12:14:44 +0800

    starter code

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//EventListener for search button
+
+//fetch weather 
+
+//display weather
+//presented with the city name, the date, an icon representation 
+//of weather conditions, the temperature, the humidity, and the the wind speed
+
+//local storage to save search result and create location button
