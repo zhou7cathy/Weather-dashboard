449034cdfebb65bd26a2c8af7698b0ea179339e3
unknown <zhou7cathy@gmail.com>
2022-12-18 12:11:39 +0800

    add condition to fetch weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,26 @@ function handleSearchFormSubmit(event) {
     alert('You need a search input value!');
     return;
   }
+
+  //fetch weather 
+	fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchInputVal + '&appid=ebd7da536e83a54f2481bdf80529f379')
+    .then(function (response) {
+			if (response.status !== 200) {
+        alert('City not found, please try again.')
+      }
+      return response.json();
+		})
+    .then(function (data) {
+      console.log(data);
+    });
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
-//fetch weather 
-fetch(
-    'https://api.openweathermap.org/data/2.5/forecast?q=perth&appid=ebd7da536e83a54f2481bdf80529f379'
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
 
 //display weather
 //presented with the city name, the date, an icon representation 
 //of weather conditions, the temperature, the humidity, and the the wind speed
+
 
 //local storage to save search result and create location button
