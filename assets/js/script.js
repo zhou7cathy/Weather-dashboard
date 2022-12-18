e6e17a8a273ef8aec7f0608dedbab52b5a8257c9
unknown <zhou7cathy@gmail.com>
2022-12-18 22:19:04 +0800

    fetch and append weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
 //EventListener for search button
 var searchFormEl = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var cityList = document.querySelector('#city-list');
+var currentWeather = document.querySelector('#current-weather');
+var weatherForecast = document.querySelector('#weather-forecast');
 
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
-  var searchInputVal = document.querySelector('#search-input').value;
-
+  var searchInputVal = searchInput.value;
   if (!searchInputVal) {
     alert('You need a search input value!');
     return;
   }
 
-  //fetch weather 
-	fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchInputVal + '&appid=ebd7da536e83a54f2481bdf80529f379')
+  //fetch & display weather 
+  //presented with the city name, the date, an icon representation of 
+  //weather conditions, the temperature, the humidity, and the the wind speed
+	fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchInputVal + '&cnt=6&appid=ebd7da536e83a54f2481bdf80529f379')
     .then(function (response) {
 			if (response.status !== 200) {
         alert('City not found, please try again.')
@@ -21,15 +26,41 @@ function handleSearchFormSubmit(event) {
 		})
     .then(function (data) {
       console.log(data);
+      var cityName = document.createElement('h2');
+      cityName.textContent = data.city.name;
+    
+      var CurrentTemperature = document.createElement('p');
+        var CurrentWind = document.createElement('p');
+        var CurrentHumidity = document.createElement('p');
+
+        CurrentTemperature.textContent = "Temp: " + data.list[0].main.temp + "°F";
+        CurrentWind.textContent = "Wind: " + data.list[0].wind.gust + "MPH";
+        CurrentHumidity.textContent = "Humidity: " + data.list[0].main.humidity + "%";
+        
+        currentWeather.append(cityName);
+        currentWeather.append(CurrentTemperature);
+        currentWeather.append(CurrentWind);
+        currentWeather.append(CurrentHumidity);
+
+      for (var i = 1; i < data.list.length; i++) {
+        var tempContainer = document.createElement('div');
+
+        var temperature = document.createElement('p');
+        var wind = document.createElement('p');
+        var humidity = document.createElement('p');
+
+        temperature.textContent ="Temp: " + data.list[i].main.temp + "°F";        
+        wind.textContent = "Wind: " + data.list[i].wind.gust + "MPH";
+        humidity.textContent = "Humidity: "+ data.list[i].main.humidity + "%";
+        
+        tempContainer.append(temperature);
+        tempContainer.append(wind);
+        tempContainer.append(humidity);
+        weatherForecast.append(tempContainer);
+      }
     });
 }
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
-
-//display weather
-//presented with the city name, the date, an icon representation 
-//of weather conditions, the temperature, the humidity, and the the wind speed
-
+  searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
 //local storage to save search result and create location button
