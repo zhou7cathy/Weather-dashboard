0ec820e8238f1016f35b2be830a807c38cf8fa45
unknown <zhou7cathy@gmail.com>
2022-12-18 11:51:29 +0800

    add eventListener for search button, fetch weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
 //EventListener for search button
+var searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+
+  if (!searchInputVal) {
+    alert('You need a search input value!');
+    return;
+  }
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
 //fetch weather 
+fetch(
+    'https://api.openweathermap.org/data/2.5/forecast?q=perth&appid=ebd7da536e83a54f2481bdf80529f379'
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 
 //display weather
 //presented with the city name, the date, an icon representation 
