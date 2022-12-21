2526c4bc83eef1f349c5c1c100028342292dadd7
unknown <zhou7cathy@gmail.com>
2022-12-21 14:37:45 +0800

    fetch dates and weather from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ var searchInput = document.querySelector('#search-input');
 var cityList = document.querySelector('#city-list');
 var currentWeather = document.querySelector('#current-weather');
 var weatherForecast = document.querySelector('#weather-forecast');
-var today = dayjs().format('DD/MM/YYYY')
 
 function handleSearchFormSubmit(event) {
   event.preventDefault();
@@ -18,7 +17,7 @@ function handleSearchFormSubmit(event) {
   //fetch & display weather 
   //presented with the city name, the date, an icon representation of 
   //weather conditions, the temperature, the humidity, and the the wind speed
-	fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchInputVal + '&cnt=6&units=metric&appid=ebd7da536e83a54f2481bdf80529f379')
+	fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchInputVal + '&units=metric&appid=ebd7da536e83a54f2481bdf80529f379')
     .then(function (response) {
 			if (response.status !== 200) {
         alert('City not found, please try again.')
@@ -28,42 +27,46 @@ function handleSearchFormSubmit(event) {
     .then(function (data) {
       console.log(data);
       var cityName = document.createElement('h2');
-      cityName.textContent = data.city.name + " (" + today + ")";
-    
+      cityName.textContent = data.city.name;
+      
       var CurrentTemperature = document.createElement('p');
-        var CurrentWind = document.createElement('p');
-        var CurrentHumidity = document.createElement('p');
+      var CurrentDate = document.createElement('p');
+      var CurrentWind = document.createElement('p');
+      var CurrentHumidity = document.createElement('p');
 
-        CurrentTemperature.textContent = "Temp: " + data.list[0].main.temp + " °C";
-        CurrentWind.textContent = "Wind: " + data.list[0].wind.speed + " m/s";
-        CurrentHumidity.textContent = "Humidity: " + data.list[0].main.humidity + " %";
+      CurrentTemperature.textContent = "Temp: " + data.list[0].main.temp + " °C";
+      CurrentDate.textContent =data.list[0].dt_txt;
+      CurrentWind.textContent = "Wind: " + data.list[0].wind.speed + " m/s";
+      CurrentHumidity.textContent = "Humidity: " + data.list[0].main.humidity + " %";
         
-        currentWeather.append(cityName);
-        currentWeather.append(CurrentTemperature);
-        currentWeather.append(CurrentWind);
-        currentWeather.append(CurrentHumidity);
+      currentWeather.append(cityName);
+      currentWeather.append(CurrentDate);
+      currentWeather.append(CurrentTemperature);
+      currentWeather.append(CurrentWind);
+      currentWeather.append(CurrentHumidity);
 
-        currentWeather.style.margin = "0 0 20px 50px";
-        currentWeather.style.borderStyle = "solid";
+      currentWeather.style.margin = "0 0 20px 50px";
+      currentWeather.style.borderStyle = "solid";
         currentWeather.style.padding = "5px";
 
-      for (var i = 1; i < data.list.length; i++) {
+      for (var i = 0; i < data.list.length; i=i+8) {
         var tempContainer = document.createElement('div');
 
+        var date = document.createElement('p');
         var temperature = document.createElement('p');
         var wind = document.createElement('p');
         var humidity = document.createElement('p');
 
+        date.textContent =data.list[i].dt_txt;
         temperature.textContent ="Temp: " + data.list[i].main.temp + " °C";        
         wind.textContent = "Wind: " + data.list[i].wind.speed + " m/s";
         humidity.textContent = "Humidity: "+ data.list[i].main.humidity + " %";
         
+        tempContainer.append(date);
         tempContainer.append(temperature);
         tempContainer.append(wind);
         tempContainer.append(humidity);
         weatherForecast.append(tempContainer);
-
-
       }
     });
 }
